477e915e58ee3b57acddaf0053780b8113f8aa16
Andrew Gait <andrew.gait@manchester.ac.uk>
2018-12-31 02:04:18 +0000

    more attempts to sort out day 25

--- adventofcode2018/advent25/advent25_again.py
+++ adventofcode2018/advent25/advent25_again.py
@@ -2,10 +2,12 @@
 
 # Trying this again with a slightly different idea
 # This is a lot faster than the other one but it doesn't get quite the right answer yet...
+# ... still not quite right even with the extra attempts to combine neighbours.  For some
+# reason it now goes below the correct answer of 420.
 
 # open file
 input = open("advent25_input.txt", "r")
-#input = open("advent25_test_input1.txt", "r")
+#input = open("advent25_test_input4.txt", "r")
 
 
 def distance(coordA, coordB):
@@ -55,11 +57,47 @@ for line in input:
 #                        print('add ',  k, constellations[add_to[j]][k])
                         constellations[add_to[0]].append(constellations[add_to[j]][k])
 
-            for j in range(1, len(add_to)):
-#                print('delete ', j)
-                del constellations[add_to[j]-(j-1)]
+            if (len(add_to) > 0):
+                for j in range(1, len(add_to)):
+#                    print('delete ', j)
+                    del constellations[add_to[j]-(j-1)]
 
 print(constellations)
 
 n_cons = len(constellations)
 print('there are ', n_cons, ' constellations')
+
+join_together = set()
+for n in range(n_cons-1):
+    for m in range(n+1, n_cons):
+        if (n != m):
+            for i in range(len(constellations[n])):
+                for j in range(len(constellations[m])):
+                    if (distance(constellations[n][i], constellations[m][j]) <= 3):
+                        join_together.add((n,m))
+
+print(join_together)
+join_list = list(join_together)
+
+while (len(join_list) > 0):
+    for n in range(len(join_list)):
+        for i in range(len(constellations[join_list[n][1]])):
+            constellations[join_list[n][0]].append(constellations[join_list[n][1]][i])
+
+    for n in range(len(join_list)):
+        del constellations[join_list[n][1]-n]
+
+    n_cons = len(constellations)
+    print('there are ', n_cons, ' constellations')
+
+    join_together = set()
+    for n in range(n_cons-1):
+        for m in range(n+1, n_cons):
+            if (n != m):
+                for i in range(len(constellations[n])):
+                    for j in range(len(constellations[m])):
+                        if (distance(constellations[n][i], constellations[m][j]) <= 3):
+                            join_together.add((n,m))
+
+    print(join_together)
+    join_list = list(join_together)
