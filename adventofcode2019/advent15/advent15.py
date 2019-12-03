48336096a02959f6c07b7d5679091214bdcb8de8
Andrew Gait <andrew.gait@manchester.ac.uk>
2019-12-03 14:30:32 +0000

    update setupfolders and run it...

--- adventofcode2019/advent15/advent15.py
+++ adventofcode2019/advent15/advent15.py
@@ -3,5 +3,22 @@
 # open file
 input = open("advent15_input.txt", "r")
 
+input_array = []
 # read string into array
 for line in input:
+    input_array.append(line)
+
+def part1():
+
+    answer = 0
+
+    return answer
+
+def part2():
+
+    answer = 0
+
+    return answer
+
+print("Part 1 answer: ", part1())
+print("Part 2 answer: ", part2())
