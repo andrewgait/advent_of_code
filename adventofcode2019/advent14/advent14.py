4e90d3294a39f59f7edeb6b1d9ad16c5c9967a1d
Andrew Gait <andrew.gait@manchester.ac.uk>
2019-12-14 22:41:10 +0000

    aoc2019 day 14, kind of going in the right direction but still unsure

--- adventofcode2019/advent14/advent14.py
+++ adventofcode2019/advent14/advent14.py
@@ -9,13 +9,17 @@ input = open("advent14_test_input.txt", "r")
 # input = open("advent14_test_input5.txt", "r")
 
 required_dict = dict()
+rhs_dict = dict()
 
 # read string into array
 for line in input:
     input_line = line[:-1].split(" => ")
     required_dict[input_line[1]] = input_line[0]
+    rhs_line = input_line[1].split(" ")
+    rhs_dict[rhs_line[1]] = int(rhs_line[0])
 
 print(required_dict)
+print(rhs_dict)
 print(required_dict["1 FUEL"])
 
 # really not sure how to do this.  not awake enough.  make another dict which determines which ids depend on others?
@@ -23,28 +27,40 @@ print(required_dict["1 FUEL"])
 def get_list(str):
     return str.split(", ")
 
-def get_id_and_value(element):
+def get_id(element):
     split = element.split(" ")
-    return int(split[0]), split[1]
+    return split[1]
 
-def part1(required_dict):
+def get_value(element):
+    split = element.split(" ")
+    return int(split[0])
+
+# some sort of recursive function is needed to do this
+def get_stuff(id, value, total, required_dict, rhs_dict):
+    keystr = str(value)+" "+id
+    list = get_list(required_dict[keystr])
+    for n in range(len(list)):
+        acc_value = get_value(list[n])
+        id = get_id(list[n])
+        if (id=="ORE"):
+            print("reached ORE, ", acc_value, value, id)
+            total += acc_value 
+        else:
+            new_value = rhs_dict[id]
+            print("ID acc_value value new_value ", id, acc_value, value, new_value)
+            total += get_stuff(id, new_value, total, required_dict, rhs_dict)
+
+    return total
+
+def part1(required_dict, rhs_dict):
 
     id = "FUEL"
     value = 1
+    total = 0
 
-    list = get_list(required_dict["1 FUEL"])
+    total += get_stuff(id, value, total, required_dict, rhs_dict)
 
-    print(list)
-    for n in range(len(list)):
-#         print(list[n])
-        value, id = get_id_and_value(list[n])
-        print(value, id)
-
-
-
-    answer = 0
-
-    return answer
+    return total
 
 def part2():
 
@@ -52,5 +68,5 @@ def part2():
 
     return answer
 
-print("Part 1 answer: ", part1(required_dict))
+print("Part 1 answer: ", part1(required_dict, rhs_dict))
 print("Part 2 answer: ", part2())
