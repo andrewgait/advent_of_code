66b359b2856858222b47f133b37045ea917a3172
Andrew Gait <andrew.gait@manchester.ac.uk>
2019-12-15 15:09:32 +0000

    aoc2019 day 15 start of code

--- adventofcode2019/advent15/advent15.py
+++ adventofcode2019/advent15/advent15.py
@@ -1,24 +1,260 @@
-# Advent of code, day 15
+# Advent of code 2019, day 15
+import numpy as np
+
+# import matplotlib.pyplot as plt
+# import matplotlib.animation as animation
 
 # open file
 input = open("advent15_input.txt", "r")
+# input = open("advent15_test_input.txt", "r")
+# input = open("advent15_test_input2.txt", "r")
+# input = open("advent15_test_input3.txt", "r")
 
-input_array = []
+input_string = None
+
 # read string into array
 for line in input:
-    input_array.append(line)
+    input_string = line
 
-def part1():
+input_list = input_string.split(",")
+input_data = []
+for i in range(len(input_list)):
+    input_data.append(int(input_list[i]))
 
-    answer = 0
+print(len(input_data))
+codelen = len(input_data)
 
-    return answer
+# extra memory space needed?
+for i in range(2000):
+    input_data.append(0)
 
-def part2():
+# make a separate copy for part2
+part2_input_data = []
+for i in range(len(input_data)):
+    part2_input_data.append(input_data[i])
+# print(input_data)
 
-    answer = 0
+def get_operating_values(input_data, op_pos, par1, par2, par3, relative_base):
+    if par1 == 2:
+        loc1 = input_data[op_pos+1]+relative_base
+    else:
+        loc1 = input_data[op_pos+1]
+    if par2 == 2:
+        loc2 = input_data[op_pos+2]+relative_base
+    else:
+        loc2 = input_data[op_pos+2]
 
-    return answer
+#     print(op_pos, par1, par2)
+#     print(loc1, loc2)
 
-print("Part 1 answer: ", part1())
-print("Part 2 answer: ", part2())
+    if par1 == 1:
+        val1 = loc1
+    else:
+        val1 = input_data[loc1]
+    if par2 == 1:
+        val2 = loc2
+    else:
+        val2 = input_data[loc2]
+
+    if par3 == 2:
+        val3 = input_data[op_pos+3] + relative_base
+    else:
+        val3 = input_data[op_pos+3]
+
+    return val1, val2, val3
+
+def get_value(base_input_data, input, op_pos, relative_base):
+    input_data = []
+    for i in range(len(base_input_data)):
+        input_data.append(base_input_data[i])
+
+    outputs = []
+
+    input_read = False
+
+    while op_pos < len(input_data):
+
+        opcode = input_data[op_pos]
+#         print('opcode ', opcode)
+        par3 = (opcode // 10**4)
+        next = opcode - (par3 * 10**4)
+        par2 = (next // 10**3)
+        next = next - (par2 * 10**3)
+        par1 = (next // 10**2)
+        next = next - (par1 * 10**2)
+        instruction = next
+
+        if (instruction==1):
+            # add
+            val1, val2, val3 = get_operating_values(
+                input_data, op_pos, par1, par2, par3, relative_base)
+
+            store_at = val3
+
+
+            value = val1 + val2
+            input_data[store_at] = value
+            op_pos += 4
+        elif (instruction==2):
+            # multiply
+            val1, val2, val3 = get_operating_values(
+                input_data, op_pos, par1, par2, par3, relative_base)
+
+            store_at = val3
+
+            value = val1 * val2
+            input_data[store_at] = value
+            op_pos += 4
+        elif (instruction==3):
+            # input value
+            if not input_read:
+                store_at = input_data[op_pos+1]
+                if par1 == 2:
+                    store_at = relative_base + input_data[op_pos+1]
+                print('instruction3, input, outputs: ', input, outputs)
+                input_data[store_at] = input
+                op_pos += 2
+                input_read = True
+            else:
+                break
+        elif (instruction==4):
+            # output value
+            value_at = input_data[op_pos+1]
+            if par1 == 0:
+                output = input_data[value_at]
+            elif par1 == 2:
+                output = input_data[value_at+relative_base]
+            else:
+                output = value_at
+            op_pos += 2
+            # in this case the output needs to be returned, not appended ?
+            outputs.append(output)
+            # when it gets to two outputs, send it
+#             if len(outputs)==2:
+#                 return outputs, op_pos, relative_base, input_data
+        elif (instruction==5):
+            # jump if non-zero
+            val1, val2, val3 = get_operating_values(
+                input_data, op_pos, par1, par2, par3, relative_base)
+
+            if val1 != 0:
+                op_pos = val2
+            else:  # "Do nothing" i.e. move on to the next instruction
+                op_pos += 3
+        elif (instruction==6):
+            # jump if zero
+            val1, val2, val3 = get_operating_values(
+                input_data, op_pos, par1, par2, par3, relative_base)
+
+            if val1 == 0:
+                op_pos = val2
+            else:  # "Do nothing" i.e. move on to the next instruction
+                op_pos += 3
+        elif (instruction==7):
+            # if 1 less than 2
+            val1, val2, val3 = get_operating_values(
+                input_data, op_pos, par1, par2, par3, relative_base)
+            store_at = val3
+
+            if (val1 < val2):
+                input_data[store_at] = 1
+            else:
+                input_data[store_at] = 0
+            op_pos += 4
+        elif (instruction==8):
+            # if equals
+            val1, val2, val3 = get_operating_values(
+                input_data, op_pos, par1, par2, par3, relative_base)
+
+            store_at = val3
+
+            if (val1 == val2):
+                input_data[store_at] = 1
+            else:
+                input_data[store_at] = 0
+            op_pos += 4
+        elif (instruction==9):
+            # adjust the relative base
+            if par1 == 1:
+                relative_base += input_data[op_pos+1]
+            elif par1 == 0:
+                relative_base += input_data[input_data[op_pos+1]]
+            else:
+                relative_base += input_data[input_data[op_pos+1]+relative_base]
+
+            op_pos += 2
+        elif (instruction==99):
+            break
+
+    return outputs, op_pos, relative_base, input_data
+
+def draw_grid(grid):
+    for j in range(len(grid)):
+        str = ""
+        for i in range(len(grid[j])):
+            if grid[j][i] == 0:
+                str += "."
+            elif grid[j][i] == 1:
+                str += "W"
+            elif grid[j][i] == 2:
+                str += "B"
+            elif grid[j][i] == 3:
+                str += "P"
+            elif grid[j][i] == 4:
+                str += "O"
+        print(str)
+
+    print("\n")
+
+
+def part1(input_data, relative_base):
+
+    op_pos = 0
+    input = 0  # seems from instructions as though this isn't needed?
+    outputs, op_pos, relative_base, input_data = get_value(
+        input_data, input, op_pos, relative_base)
+
+    print(op_pos, relative_base, len(outputs), outputs)
+
+    count_length = 0
+
+    return count_length
+
+
+def part2(input_data, relative_base):
+
+    op_pos = 0
+    input_data[0] = 2
+    input = 0
+    outputs, op_pos, relative_base, input_data1 = get_value(
+        input_data, input, op_pos, relative_base)
+
+    print(op_pos, relative_base, len(outputs), outputs)
+
+    score = 0
+
+    return score
+
+def test_robot_movement():
+    #
+    # we need to move a robot around a grid
+    size = 10
+    grid = np.zeros((size,size), dtype=np.int32)
+
+    start_at = (size//2, size//2)
+
+    draw_grid(grid)
+
+
+test_robot_movement()
+
+relative_base = 0
+print(len(input_data), input_data)
+print('Part1: ', part1(input_data, relative_base))
+print('\n')
+
+relative_base = 0
+print(' part2 input ', len(part2_input_data))
+print(part2_input_data)
+score = part2(part2_input_data, relative_base)
+print('Part2: ', score)
