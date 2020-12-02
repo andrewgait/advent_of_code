7245ad5fd52acdf8c969a5497728781921bdce5b
Andrew Gait <andrew.gait@manchester.ac.uk>
2020-12-02 08:56:38 +0000

    AoC 2020 Day 2 solution

--- adventofcode2020/advent2/advent2.py
+++ adventofcode2020/advent2/advent2.py
@@ -2,6 +2,7 @@
 
 # open file
 input = open("advent2_input.txt", "r")
+# input = open("advent2_test_input.txt", "r")
 
 input_array = []
 # read string into array
@@ -10,15 +11,59 @@ for line in input:
 
 def part1():
 
-    answer = 0
+#     answer = 0
+    n = len(input_array)
+    print('Number of passwords is ', n)
+    n_valid = 0
+    for i in range(n):
+        # format is N-M *: password
+        this_line = input_array[i]
+        splitcolon = this_line[:-1].split(":")
+        password = splitcolon[1][1:]
+        splitspace = splitcolon[0].split(" ")
+        letter = splitspace[1]
+        splitdash = splitspace[0].split("-")
+        low = int(splitdash[0])
+        high = int(splitdash[1])
+        n_letters = 0
+        for j in range(len(password)):
+            if password[j] == letter:
+                n_letters += 1
 
-    return answer
+#         print(letter, ' appears ', n_letters, ' times in ', password)
+
+        if (n_letters >= low) and (n_letters <= high):
+            n_valid += 1
+
+    return n_valid
 
 def part2():
 
     answer = 0
+    n = len(input_array)
+    print('Number of passwords is ', n)
+    n_valid = 0
+    n_invalid = 0
+    for i in range(n):
+        # format is N-M *: password
+        this_line = input_array[i]
+        splitcolon = this_line[:-1].split(":")
+        password = splitcolon[1][1:]
+        splitspace = splitcolon[0].split(" ")
+        letter = splitspace[1]
+        splitdash = splitspace[0].split("-")
+        low = int(splitdash[0])
+        high = int(splitdash[1])
+        n_letters = 0
+        if ((password[low-1] == letter) and (password[high-1] == letter)):
+            n_invalid += 1
+        elif ((password[low-1] == letter) or (password[high-1] == letter)):
+            n_valid += 1
+        else:
+            n_invalid += 1
 
-    return answer
+    print(n_valid, n_invalid)
+    return n_valid
 
 print("Part 1 answer: ", part1())
 print("Part 2 answer: ", part2())
