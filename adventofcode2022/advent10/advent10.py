654bc54e23991d28c33a8a71f4170bbb8ebcd35d
Andrew Gait <andrew.gait@manchester.ac.uk>
2022-11-29 08:59:15 +0000

    Setup for 2022

---
+++ adventofcode2022/advent10/advent10.py
@@ -0,0 +1,24 @@
+# Advent of code 2022, day 10
+
+# open file
+input = open("advent10_input.txt", "r")
+
+input_array = []
+# read string into array
+for line in input:
+    input_array.append(line)
+
+def part1():
+
+    answer = 0
+
+    return answer
+
+def part2():
+
+    answer = 0
+
+    return answer
+
+print("Part 1 answer: ", part1())
+print("Part 2 answer: ", part2())
