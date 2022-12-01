ee7c25edd87da355fbc32d921f95f640fe66577f
Andrew Gait <andrew.gait@manchester.ac.uk>
2022-12-01 09:40:11 +0000

    AOC day 1 2022

--- adventofcode2022/advent1/advent1.py
+++ adventofcode2022/advent1/advent1.py
@@ -2,21 +2,55 @@
 
 # open file
 input = open("advent1_input.txt", "r")
+# input = open("advent1_test_input1.txt", "r")
 
 input_array = []
 # read string into array
 for line in input:
-    input_array.append(line)
+    input_array.append(line[:-1])
 
 def part1():
 
-    answer = 0
+    most_cals = 0
+    cals = []
+
+    cals_tot = 0
+    for input in input_array:
+        if len(input) == 0:
+            cals.append(cals_tot)
+            cals_tot = 0
+        else:
+            cals_tot += int(input)
+
+    print(cals)
+
+    answer = max(cals)
 
     return answer
 
 def part2():
 
-    answer = 0
+    most_cals = 0
+    cals = []
+
+    cals_tot = 0
+    for input in input_array:
+        if len(input) == 0:
+            cals.append(cals_tot)
+            cals_tot = 0
+        else:
+            cals_tot += int(input)
+
+    print(cals)
+
+    # I'm guessing this might run into problems if any of the values in cals are equal
+    first = max(cals)
+    cals.remove(first)
+    second = max(cals)
+    cals.remove(second)
+    third = max(cals)
+
+    answer = first + second + third
 
     return answer
 
