d695e8aa35ea96ca6b7ba5c03e704ebbd33bba6e
Andrew Gait <andrew.gait@manchester.ac.uk>
2022-12-03 21:56:29 +0000

    AOC 2022 day 3

--- adventofcode2022/advent3/advent3.py
+++ adventofcode2022/advent3/advent3.py
@@ -2,21 +2,59 @@
 
 # open file
 input = open("advent3_input.txt", "r")
+# input = open("advent3_test_input1.txt", "r")
 
 input_array = []
 # read string into array
 for line in input:
     input_array.append(line)
 
+lowercase = "abcdefghijklmnopqrstuvwxyz"
+uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+lower_dict = {}
+upper_dict = {}
+
+for n in range(len(lowercase)):
+    lower_dict[lowercase[n]] = n+1
+    upper_dict[uppercase[n]] = n+27
+
 def part1():
 
-    answer = 0
+    total = 0
+
+
+    for input in input_array:
+        input_len = len(input) - 1
+        first = input[:input_len//2]
+        second = input[input_len//2:]
+        common = list(set(first).intersection(second))
+        if common[0].isupper():
+            total += upper_dict[common[0]]
+        if common[0].islower():
+            total += lower_dict[common[0]]
+
+    answer = total
 
     return answer
 
 def part2():
 
-    answer = 0
+    total = 0
+
+    array_len = len(input_array)
+
+    for n in range(array_len//3):
+        inputs = [input_array[n*3][:-1], input_array[3*n+1][:-1],
+                  input_array[3*n+2][:-1]]
+        common = list(
+            set(inputs[0]).intersection(inputs[1]).intersection(inputs[2]))
+        if common[0].isupper():
+            total += upper_dict[common[0]]
+        if common[0].islower():
+            total += lower_dict[common[0]]
+
+    answer = total
 
     return answer
 
