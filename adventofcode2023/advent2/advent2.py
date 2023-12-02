3b20ca46a51b2462578882d5905ecb2062bb16d4
Andrew Gait <andrew.gait@manchester.ac.uk>
2023-12-02 13:41:55 +0000

    AoC 2023 day 2

--- adventofcode2023/advent2/advent2.py
+++ adventofcode2023/advent2/advent2.py
@@ -2,6 +2,7 @@
 
 # open file
 input = open("advent2_input.txt", "r")
+# input = open("advent2_input_test.txt", "r")
 
 input_array = []
 # read string into array
@@ -10,13 +11,77 @@ for line in input:
 
 def part1():
 
-    answer = 0
+    max_red = 12
+    max_green = 13
+    max_blue = 14
+
+    sum_id = 0
+
+    for input in input_array:
+        game_good = True
+        game_string = input.split(":")[0]
+        game_instructions = input.split(":")[1]
+        game_id = int(game_string.split(" ")[1])
+        # print(game_id)
+        # Ignore the \n at the end
+        games = game_instructions[:-1].split(";")
+
+        for game in games:
+            colours_inst = game.split(",")
+            for colour_inst in colours_inst:
+                colour = colour_inst[1:].split(" ")
+                if colour[1] == "red" and int(colour[0]) > max_red:
+                    game_good = False
+                    break
+                if colour[1] == "green" and int(colour[0]) > max_green:
+                    game_good = False
+                    break
+                if colour[1] == "blue" and int(colour[0]) > max_blue:
+                    game_good = False
+                    break
+            if not game_good:
+                break
+
+        if game_good:
+            sum_id += game_id
+
+
+
+    answer = sum_id
 
     return answer
 
 def part2():
 
-    answer = 0
+    sum_power = 0
+
+    for input in input_array:
+        max_red = 0
+        max_green = 0
+        max_blue = 0
+        game_good = True
+        game_string = input.split(":")[0]
+        game_instructions = input.split(":")[1]
+        game_id = int(game_string.split(" ")[1])
+        # print(game_id)
+        # Ignore the \n at the end
+        games = game_instructions[:-1].split(";")
+
+        for game in games:
+            colours_inst = game.split(",")
+            for colour_inst in colours_inst:
+                colour = colour_inst[1:].split(" ")
+                if colour[1] == "red" and int(colour[0]) > max_red:
+                    max_red = int(colour[0])
+                if colour[1] == "green" and int(colour[0]) > max_green:
+                    max_green = int(colour[0])
+                if colour[1] == "blue" and int(colour[0]) > max_blue:
+                    max_blue = int(colour[0])
+
+        power = max_red * max_green * max_blue
+        sum_power += power
+
+    answer = sum_power
 
     return answer
 
