44b2618c08a8212d5200ff046010a4fd5a3fc8d5
Andrew Gait <andrew.gait@manchester.ac.uk>
2024-12-01 14:49:58 +0000

    Uncommitted code from 2023

--- adventofcode2023/advent19/advent19.py
+++ adventofcode2023/advent19/advent19.py
@@ -2,6 +2,7 @@
 
 # open file
 input = open("advent19_input.txt", "r")
+input = open("advent19_input_test.txt", "r")
 
 input_array = []
 # read string into array
@@ -10,15 +11,209 @@ for line in input:
 
 def part1():
 
+    is_input_data = False
+    workflows = {}
+    input_dicts = []
+    for input in input_array:
+        if input == "\n":
+            is_input_data = True
+            continue
+
+        if is_input_data:
+            # Form of {x ,m, a, s}
+            splitcomma = input.split(",")
+            input_vals = {splitcomma[0][1]: int(splitcomma[0][3:]),
+                          splitcomma[1][0]: int(splitcomma[1][2:]),
+                          splitcomma[2][0]: int(splitcomma[2][2:]),
+                          splitcomma[3][0]: int(splitcomma[3][2:-2])}
+            input_dicts.append(input_vals)
+        else:
+            splitbrace = input.split("{")
+            workflow_inst = splitbrace[0]
+            splitcomma = splitbrace[1][:-2].split(",")
+            len_comma = len(splitcomma)
+            workflows[workflow_inst] = [splitcomma[n] for n in range(len_comma)]
+
+    print(workflows)
     answer = 0
+
+    for input_dict in input_dicts:
+        instruction = "in"
+        print(input_dict)
+
+        while instruction not in ["A", "R"]:
+            workflow = workflows[instruction]
+            # print(workflow, instruction)
+            for work in workflow:
+                if ":" in work:
+                    splitcolon = work.split(":")
+                    resultiftrue = splitcolon[1]
+                    valtest = splitcolon[0][0]
+                    test = splitcolon[0][1]
+                    number = int(splitcolon[0][2:])
+
+                    if test == ">":
+                        if input_dict[valtest] > number:
+                            instruction = resultiftrue
+                            break
+                        # else carry on to next instruction
+                    elif test == "<":
+                        if input_dict[valtest] < number:
+                            instruction = resultiftrue
+                            break
+                        # else carry on to next instruction
+
+                else:
+                    instruction = work
+                    break
+
+        if instruction == "A":
+            answer += sum(input_dict.values())
 
     return answer
 
 def part2():
 
+    is_input_data = False
+    workflows = {}
+    input_dicts = []
+    for input in input_array:
+        if input == "\n":
+            is_input_data = True
+            continue
+
+        if is_input_data:
+            # Form of {x ,m, a, s}
+            splitcomma = input.split(",")
+            input_vals = {splitcomma[0][1]: int(splitcomma[0][3:]),
+                          splitcomma[1][0]: int(splitcomma[1][2:]),
+                          splitcomma[2][0]: int(splitcomma[2][2:]),
+                          splitcomma[3][0]: int(splitcomma[3][2:-2])}
+            input_dicts.append(input_vals)
+        else:
+            splitbrace = input.split("{")
+            workflow_inst = splitbrace[0]
+            splitcomma = splitbrace[1][:-2].split(",")
+            len_comma = len(splitcomma)
+            workflows[workflow_inst] = [splitcomma[n] for n in range(len_comma)]
+
+    print(workflows)
     answer = 0
 
+    instruction = "in"
+
+    answer = 1
+    input_dict_lists = {}
+    for valletter in ["x", "m", "a", "s"]:
+        input_dict_lists[valletter] = [[[n for n in range(1,4001)], "in"]]
+
+    # for key, input_dict_list in input_dict_lists.items():
+    #     print(key)
+    #     for input in input_dict_list:
+    #         print(input)
+
+    all_inst_A_or_R = False
+    # print(valletter)
+
+    nnn = 0
+    while nnn < 2: #not all_inst_A_or_R:
+        nnn += 1
+        new_input_dict_lists = {}
+        for valletter in ["x", "m", "a", "s"]:
+            new_input_dict_lists[valletter] = []
+        # print(" ")
+        for valkey, input_dict_listed in input_dict_lists.items():
+            for input_dict_list in input_dict_listed:
+                instruction = input_dict_list[1]
+                # print(instruction, valkey, input_dict_list[0])
+                if ":" in instruction:
+                    instruction = instruction.split(":")[1]
+                if instruction not in ["A", "R"]:
+                    workflow = workflows[instruction]
+                    # print(workflow, instruction)
+                    n_works = len(workflow)
+                    for n_work in range(n_works):
+                        if ":" in workflow[n_work]:
+                            splitcolon = workflow[n_work].split(":")
+                            resultiftrue = splitcolon[1]
+                            valtest = splitcolon[0][0]
+                            test = splitcolon[0][1]
+                            number = int(splitcolon[0][2:])
+
+                            if valtest != valkey:
+                                new_input_dict_lists[valkey].append(
+                                    [input_dict_list[0], workflow[n_work+1]])
+                            else:
+                                if test == ">":
+                                    new_above_list = []
+                                    new_below_list = []
+                                    for nn in range(len(input_dict_list[0])):
+                                        if input_dict_list[0][nn] > number:
+                                            new_above_list.append(input_dict_list[0][nn])
+                                        else:
+                                            new_below_list.append(input_dict_list[0][nn])
+
+                                    new_input_dict_lists[valkey].append(
+                                        [new_below_list, workflow[n_work+1]])
+                                    if resultiftrue != "R":
+                                        new_input_dict_lists[valkey].append(
+                                            [new_above_list, resultiftrue])
+                                elif test == "<":
+                                    new_above_list = []
+                                    new_below_list = []
+                                    for nn in range(len(input_dict_list[0])):
+                                        if input_dict_list[0][nn] < number:
+                                            new_below_list.append(input_dict_list[0][nn])
+                                        else:
+                                            new_above_list.append(input_dict_list[0][nn])
+
+                                    new_input_dict_lists[valkey].append(
+                                        [new_above_list, workflow[n_work+1]])
+                                    if resultiftrue != "R":
+                                        new_input_dict_lists[valkey].append(
+                                            [new_below_list, resultiftrue])
+
+                        # else:
+                        #     new_input_dict_lists[valkey].append([input_dict_list[0], workflow[n_work]])
+                else: # instruction is A or R, just copy it
+                    if instruction == "A":
+                        new_input_dict_lists[valkey].append(
+                            [input_dict_list[0], input_dict_list[1][-1]])
+                    # elif instruction == "R":
+
+
+        input_dict_lists = new_input_dict_lists.copy()
+        print("check instructions")
+        for key, input_dict_list in input_dict_lists.items():
+            print(key)
+            for input in input_dict_list:
+                print(input)
+
+        all_inst_A_or_R = True
+        for _, new_inputs in new_input_dict_lists.items():
+            for new_input in new_inputs:
+                if new_input[1] not in ["A", "R"]:
+                    all_inst_A_or_R = False
+                    break
+
+    # print("final dict list for ", valletter)
+
+    final_set = set()
+    # print(input_dict_lists)
+    for _, input_dict_listed in input_dict_lists.items():
+        # print(input_dict_listed)
+        for input_dict_list in input_dict_listed[0]:
+            for n in range(len(input_dict_list)):
+                final_set.add(input_dict_list[n])
+    # print(final_set)
+
+    answer *= len(final_set)
+
+    answer = (4000-2662)*(4000-(2090-1801))*(1716)*(2770-1351)
+
     return answer
+
+    return input_dict_lists
 
 print("Part 1 answer: ", part1())
 print("Part 2 answer: ", part2())
--- adventofcode2023/advent21/advent21.py
+++ adventofcode2023/advent21/advent21.py
@@ -1,22 +1,251 @@
 # Advent of code 2023, day 21
+import numpy as np
+from collections import deque
+import networkx as netx
 
 # open file
 input = open("advent21_input.txt", "r")
+input = open("advent21_input_test.txt", "r")
 
 input_array = []
 # read string into array
 for line in input:
     input_array.append(line)
 
+garden_dict = {".": 0, "#": 1, "O": 2, "S": 3}
+garden_rev_dict = {0: ".", 1: "#", 2: "O", 3: "S"}
+
+def print_garden(garden_array, start):
+    ny = len(garden_array)
+    nx = len(garden_array[0])
+    for y in range(ny):
+        garden_line = ""
+        for x in range(nx):
+            if start[0] == y and start[1] == x and (garden_array[y][x] != garden_dict["O"]):
+                garden_line += "S"
+            else:
+                garden_line += garden_rev_dict[garden_array[y][x]]
+        print(garden_line)
+    print(" ")
+
+
 def part1():
 
+    nx = 0
+    ny = 0
+    start = [0,0]
+    garden_array = []
+    for input in input_array:
+        garden_array_line = []
+        nx = 0
+        for inp in input[:-1]:
+            if inp == "S":
+                start = [ny,nx]
+            garden_array_line.append(garden_dict[inp])
+            nx += 1
+        garden_array.append(garden_array_line)
+        ny += 1
+
+    garden_array = np.array(garden_array)
+
+    print_garden(garden_array, start)
+
+    garden_array[start[0]][start[1]] = garden_dict["O"]
+
+    n_steps = 6
+
+    for n in range(n_steps):
+        new_garden_array = np.zeros((ny,nx))
+        new_Os = []
+        for y in range(ny):
+            new_garden_array_line = []
+            for x in range(nx):
+                if garden_array[y][x] == garden_dict["O"]:
+                    # Set it to .
+                    garden_array[y][x] == garden_dict["."]
+                    # Check in every direction around this for .
+                    if x-1 >= 0:
+                        if garden_array[y][x-1] == garden_dict["."]:
+                            new_garden_array[y][x-1] = garden_dict["O"]
+                            new_Os.append([y,x-1])
+                    if y-1 >= 0:
+                        if garden_array[y-1][x] == garden_dict["."]:
+                            new_garden_array[y-1][x] = garden_dict["O"]
+                            new_Os.append([y-1,x])
+                    if x+1 < nx:
+                        if garden_array[y][x+1] == garden_dict["."]:
+                            new_garden_array[y][x+1] = garden_dict["O"]
+                            new_Os.append([y,x+1])
+                    if y+1 < ny:
+                        if garden_array[y+1][x] == garden_dict["."]:
+                            new_garden_array[y+1][x] = garden_dict["O"]
+                            new_Os.append([y+1,x])
+                else:
+                    if [y,x] not in new_Os:
+                        new_garden_array[y][x] = garden_array[y][x]
+
+
+        garden_array = new_garden_array.copy()
+        n_possible = 0
+        for y in range(ny):
+            for x in range(nx):
+                if garden_array[y][x] == garden_dict["O"]:
+                    n_possible += 1
+        print("Step ", n+1, n_possible)
+
+    print_garden(new_garden_array, start)
+
     answer = 0
+    for y in range(ny):
+        for x in range(nx):
+            if garden_array[y][x] == garden_dict["O"]:
+                answer += 1
+
 
     return answer
 
 def part2():
 
+    nx = 0
+    ny = 0
+    start = [0,0]
+    garden_array = []
+    for input in input_array:
+        garden_array_line = []
+        nx = 0
+        for inp in input[:-1]:
+            if inp == "S":
+                start = [ny,nx]
+                garden_array_line.append(garden_dict["."])
+            else:
+                garden_array_line.append(garden_dict[inp])
+            nx += 1
+        garden_array.append(garden_array_line)
+        ny += 1
+
+    garden_array = np.array(garden_array)
+
+    print_garden(garden_array, start)
+
+    # n_steps = 1000
+
+    # Feels like this must be possible using a deque in some way
+    # though how can that be made quadruple-ended rather than double-ended?
+
+    # The first step's answer is contained in the third step's answer, which
+    # is contained in the fifth step's answer, and so on.
+
+    # The below works for the low values but it's not viable for the final answer
+
+    moves = [[-1,0],[0,1],[0,-1],[1,0]]
+
+    n_steps = 10
+
+    Os = [start]
+    new_Os = set()
+    new_Os.add((start[0],start[1]))
+    # new_Os = deque()
+    # new_Os.append((start[0],start[1]))
+    for n in range(n_steps//2):
+        # # new_Os = set()
+        # # print(new_Os, Os)
+        # # newly_added_Os = []
+        # for Opt in Os:
+        #     osy = Opt[0]
+        #     osx = Opt[1]
+        #     for move1 in moves:
+        #         if garden_array[(osy+move1[0]) % ny][(osx+move1[1]) % nx] == garden_dict["."]:
+        #             for move2 in moves:
+        #                 if garden_array[(osy+move1[0]+move2[0]) % ny][(osx+move1[1]+move2[1]) % nx] == garden_dict["."]:
+        #                     # new_Os.append((osy+move1[0]+move2[0], osx+move1[1]+move2[1]))
+        #                     new_pt = (osy+move1[0]+move2[0], osx+move1[1]+move2[1])
+        #                     new_Os.add(new_pt)
+        #                     # if new_pt not in Os:
+        #                     #     newly_added_Os.append(new_pt)
+        #
+        # # Os = list(newly_added_Os)
+        # Os = list(new_Os)
+        # print("Step ", 2*(n+1), len(new_Os))
+        my_answer = do_moves(garden_array, nx, ny, moves, start[1], start[0], 0, 2*(n+1), new_Os)
+
+        print("Step ", 2*(n+1), len(my_answer))  #, my_answer)
+
+    # Sadly doing this is also far too slow and breaks the maximum recursion depth
+    # answer = do_moves(garden_array, nx, ny, moves, start[1], start[0], 0, 2*(5000+1), new_Os)
+     # len(my_answer)
+
+    # Perhaps it's also possible that it can be done using graphs
+    # If we go from start and make graphs whereby the weight of each # is max(n_steps) +  1
+    # and the weight of each . is 1, then counting the number of paths with an even length
+    # where the weight is less than max(n_steps) will give the answer?
+
+    garden_graph = netx.Graph()
+
+    n_steps = 1000
+
+    # Add nodes
+    for y in range(-n_steps, n_steps+1):
+        for x in range(-n_steps, n_steps+1):
+            test_y = start[0] + y
+            test_x = start[0] + x
+            nodestr = str(test_y)+"_"+str(test_x)
+            garden_graph.add_node(nodestr) #, garden_array[test_y % ny][test_x % nx])
+
+    print(netx.number_of_nodes(garden_graph))
+
+    # Add edges
+    for y in range(-n_steps, n_steps):
+        for x in range(-n_steps, n_steps):
+            test_y = start[0] + y
+            test_x = start[0] + x
+            nodestr = str(test_y)+"_"+str(test_x)
+            test_y_1 = start[0] + y + 1
+            nodedownstr = str(test_y_1)+"_"+str(test_x)
+            test_x_1 = start[0] + x + 1
+            noderightstr = str(test_y)+"_"+str(test_x_1)
+            if ((garden_array[test_y % ny][test_x % nx] == garden_dict["."]) and
+                (garden_array[test_y_1 % ny][test_x % nx]) == garden_dict["."]):
+                garden_graph.add_edge(nodestr, nodedownstr, weight=1)
+            else:
+                garden_graph.add_edge(nodestr, nodedownstr, weight=n_steps+1)
+            if ((garden_array[test_y % ny][test_x % nx] == garden_dict["."]) and
+                (garden_array[test_y % ny][test_x_1 % nx]) == garden_dict["."]):
+                garden_graph.add_edge(nodestr, noderightstr, weight=1)
+            else:
+                garden_graph.add_edge(nodestr, noderightstr, weight=n_steps+1)
+
+    print(netx.number_of_edges(garden_graph))
+
     answer = 0
+    start_str = str(start[1])+"_"+str(start[0])
+    paths = netx.single_source_dijkstra_path(garden_graph, start_str, n_steps, "weight")
+
+    for path in paths.values():
+        path_weight = netx.path_weight(garden_graph, path, "weight")
+        if path_weight % 2 == 0:
+            answer += 1
+            print(answer, path_weight)
+
+    return answer
+
+def do_moves(garden_array, nx, ny, moves, osy, osx, depth, max_depth, answer):
+    # print("Adding value from ", osy, osx, " at ", depth)
+    answer.add((osy, osx))
+    if depth < max_depth:
+        for move1 in moves:
+            new_osy1 = (osy+move1[0])
+            new_osx1 = (osx+move1[1])
+            if garden_array[new_osy1 % ny][new_osx1 % nx] == garden_dict["."]:
+                for move2 in moves:
+                    new_osy2 = (new_osy1+move2[0])
+                    new_osx2 = (new_osx1+move2[1])
+                    if garden_array[new_osy2 % ny][new_osx2 % nx] == garden_dict["."]:
+                        # answer += 1
+                        # depth += 2
+                        answer = do_moves(garden_array, nx, ny, moves,
+                                          new_osy2, new_osx2, depth+2, max_depth, answer)
+
+        depth += 2
 
     return answer
 
